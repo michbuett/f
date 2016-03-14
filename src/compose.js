0c216d4a9dd2570056b07c28256f3bcad97785a2
Michael Büttner <michael.buettner@flyeralarm.com>
2016-03-14 20:02:07 +0100

    Allow composing more than 2 functions

--- src/compose.js
+++ src/compose.js
@@ -1,7 +1,13 @@
 // compose :: fn... -> fn
-module.exports = function compose(f, g) {
+module.exports = function compose(...args) {
 
     return function (val) {
-        return f(g(val));
+        let result = val;
+
+        for (let i = args.length - 1; i >= 0; i--) {
+            result = args[i](result);
+        }
+
+        return result;
     };
 };
--- tests/compose.spec.js
+++ tests/compose.spec.js
@@ -2,10 +2,21 @@ describe('compose', function () {
     'use strict';
 
     var compose = require('../compose');
-    var foo = function (x) { return 'oof' + x + 'foo'; };
-    var bar = function (x) { return 'rab' + x + 'bar'; };
+    var f = function (x) { return 'f(' + x + ')'; };
+    var g = function (x) { return 'g(' + x + ')'; };
+    var h = function (x) { return 'h(' + x + ')'; };
 
     it('returns a function', function () {
-        expect(typeof compose(foo, bar)).toBe('function');
+        expect(typeof compose(f, g)).toBe('function');
+    });
+
+    it('allows all function (right to left)', function () {
+        var foobar = compose(f, g);
+        expect(foobar('x')).toBe('f(g(x))');
+    });
+
+    it('allows to compose more than two functions', function () {
+        var many = compose(f, g, h, f, g, h);
+        expect(many('x')).toBe('f(g(h(f(g(h(x))))))');
     });
 });
