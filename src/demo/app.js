124e648b20832deb7d917cbb8dfef9b2bf325755
michbuett <michbuett@gmx.de>
2016-03-25 19:46:20 +0100

    Improve demo app: REPL-man

--- src/demo/app.js
+++ src/demo/app.js
@@ -1,41 +1,76 @@
+/* eslint no-console: 0 */
 module.exports = (function () {
     'use strict';
 
+    const STORAGE_KEY = 'DEMO_CODE';
+
+    var output = '';
+
     var Demo = function (editor, outEl) {
+        ['log', 'warn', 'error'].forEach(function (fname) {
+            var orgFn = console[fname];
+            console[fname] = function (...args) {
+                output += formatOutputItem(fname, fname.toUpperCase(), args.map((arg) => formatValue(arg)).join(' '));
+                orgFn.apply(console, args);
+            };
+        });
+
+
+        var initialCode = localStorage.getItem(STORAGE_KEY);
+        if (initialCode) {
+            handleCodeChange(initialCode, outEl);
+            editor.setValue(initialCode);
+            editor.clearSelection();
+        }
+
+        editor.focus();
+
         editor.on('change', function () {
-            outEl.innerHTML = evalContent(editor.getValue());
+            handleCodeChange(editor.getValue(), outEl);
         });
     };
 
+    return Demo;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // PRIVATE HELPER
+
     /** @private */
-    function evalContent(code) {
-        try {
-            /* jshint evil: true */
-            return formatResult(eval(code));
-            /* jshint evil: false */
-        } catch (e) {
-            return formatError(e);
-        }
+    function handleCodeChange(code, outEl) {
+        localStorage.setItem(STORAGE_KEY, code);
+        outEl.innerHTML = evalContent(code);
     }
 
     /** @private */
-    function formatResult(result) {
+    function evalContent(code) {
+        output = '';
+        try {
+            /* jshint evil: true */
+            var result = formatValue(eval(code));
+            output += formatOutputItem('result', 'RESULT', result);
+            /* jshint evil: false */
+        } catch (e) {
+            output += formatOutputItem('error', 'ERROR', e);
+        }
+        return output;
+    }
+
+    /** @private */
+    function formatValue(result) {
         if (result && typeof result === 'object' && result.constructor === Object) {
-            return '[object]\n\n' + JSON.stringify(result, null, '  ');
+            return JSON.stringify(result, null, '  ');
         }
 
         if (Array.isArray(result)) {
-            return '[array]\n\n' + JSON.stringify(result);
+            return JSON.stringify(result);
         }
 
-        return '[' + (typeof result) + ']\n\n' + result;
+        return result;
     }
+
 
     /** @private */
-    function formatError(e) {
-        return e;
+    function formatOutputItem(type, title, content) {
+        return `<div class="output-item ${type}"><div class="title">${title}</div><div class="content">${content}</div></div>`;
     }
-
-    return Demo;
-
 }());
