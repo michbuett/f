c19ad427a949885931e82525d02d99a3958f1319
michbuett <michbuett@gmx.de>
2016-07-29 20:06:20 +0200

    Make list type monadic

--- src/lib/List.js
+++ src/lib/List.js
@@ -1,24 +1,119 @@
 module.exports = (function () {
 
-    let List = function List(items) {
-        this._items = items;
-    };
+    const TYPE_ID = 'cb8a6d4b-01e5-4fe3-bc18-ac5fce6293fb';
+    const isArray = Array.isArray;
 
-    // map :: List a -> (a -> b) -> List b
-    List.prototype.map = function List_map(fn) {
-        return new List(this._items.map(fn));
-    };
+    class List {
 
-    List.prototype.toString = function List_toString() {
-        return 'List(' + JSON.stringify(this._items) + ')';
-    };
+        // List.of :: a -> List a
+        static of(items) {
+            return isArray(items) ? new List(items) : new List([items]);
+        }
 
-    List.prototype.join = function List_join() {
-        return this._items;
-    };
+        // List.is :: a -> Boolean
+        static is(any) {
+            return any && any._typeId === TYPE_ID;
+        }
 
-    // List.of :: a -> List a
-    List.of = items => new List(items);
+        constructor(items) {
+            this._items = items;
+            this._typeId = TYPE_ID;
+        }
+
+        get(index) {
+            return this._items[index];
+        }
+
+        set(...args) {
+            if (args.length === 2 && args[0] >= 0) {
+                // OPTION 1
+                // call list.set(key, value);
+
+                let [i, value] = args;
+
+                if (this._items[i] === value) {
+                    return this;
+                } else {
+                    let newList = this.toJS();
+                    newList[i] = value;
+
+                    return new List(newList);
+                }
+            }
+
+            if (args.length === 1 && isArray(args[0])) {
+                // OPTION 2
+                // call list.set([newVa1, newVal2, ...]);
+
+                let newItems = mergeLists(this._items, args[0]);
+                return this._items === newItems ? this : new List(newItems);
+            }
+
+            // OPTION 3
+            // call list.set(newVa1, newVal2, ...);
+            let newItems = mergeLists(this._items, args);
+            return this._items === newItems ? this : new List(newItems);
+        }
+
+        // toJS :: List a -> a
+        toJS() {
+            return [].concat(this._items);
+        }
+
+        // map :: List a -> (a -> b) -> List b
+        map(fn) {
+            return new List(this._items.map(fn));
+        }
+
+        join() {
+            return new List(this._items.reduce((result, items) => result.concat(items.toJS()), []));
+        }
+
+        // for debugging
+        toString() {
+            return 'List(' +  this._items.map(item => {
+                if (isArray(item)) {
+                    return '[' + item + ']';
+                }
+
+                if (typeof item === 'undefined') {
+                    return 'undefined';
+                }
+
+                if (item && item.toString !== Object.prototype.toString) {
+                    return item.toString();
+                }
+
+                return JSON.stringify(item);
+            }).join(',') + ')';
+        }
+
+        toJSON() {
+            return this.toString();
+        }
+    }
 
     return List;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // PRIVATE HELPER
+
+    /** @private */
+    function mergeLists (list1, list2) {
+        let newList = list1;
+
+        for (let i = 0, l = list2.length; i < l; i++) {
+            if (typeof list2[i] === 'undefined') {
+                continue;
+            }
+
+            if (newList[i] !== list2[i]) {
+                newList = newList === list1 ? [].concat(list1) : newList;
+                newList[i] = list2[i];
+            }
+        }
+
+        return newList;
+
+    }
 }());
--- tests/List.spec.js
+++ tests/List.spec.js
@@ -2,33 +2,107 @@ describe('List', function () {
     'use strict';
 
     var List = require('../build/test/List');
+    var map = require('../build/test/map');
+    var compose = require('../build/test/compose');
+    var id = require('../build/test/id');
+    var join = require('../build/test/join');
 
-    it('is a functor', function () {
-        expect(typeof List).toBe('function');
-        expect(typeof List.of).toBe('function');
-        expect(typeof List.of([]).map).toBe('function');
+    var l = List.of([1, 2, 3]);
+
+    describe('item list', function () {
+        it('allows to be transformed into javascript array', function () {
+            expect(l.toJS()).toEqual([1, 2, 3]);
+        });
+
+        it('allows to read store values using keys', function () {
+            expect(l.get(0)).toBe(1);
+            expect(l.get(1)).toBe(2);
+            expect(l.get(2)).toBe(3);
+        });
+
+        it('allows to set new values without mutating the list', function () {
+            // excute
+            var l1 = l.set(0, 'foo');
+            var l2 = l.set([2, undefined, 5, 6]);
+            var l3 = l.set(2, undefined, 5, 6);
+
+            // verify
+            expect(l.toJS()).toEqual([1, 2, 3]);
+            expect(l1.toJS()).toEqual(['foo', 2, 3]);
+            expect(l2.toJS()).toEqual([2, 2, 5, 6]);
+            expect(l3.toJS()).toEqual([2, 2, 5, 6]);
+        });
+
+        it('allows to map list items without mutating the list', function () {
+            // prepare
+
+            // excute
+            var result = l.map(function (x) { return 2 * x; });
+
+            // verify
+            expect(l).not.toBe(result);
+            expect(l.toJS()).toEqual([1, 2, 3]);
+            expect(result.toJS()).toEqual([2, 4, 6]);
+        });
     });
 
-    it('is a monad', function () {
-        expect(typeof List.of([]).map).toBe('function');
-        expect(typeof List.of([]).join).toBe('function');
-        expect(List.of([1, 2]).join()).toEqual([1, 2]);
+    describe('monadic nature', function () {
+        it('is pointed', function () {
+            expect(typeof List.of).toBe('function');
+            expect(List.of({ foo: 'bar' })).toBeTruthy();
+        });
+
+        it('can be flattened using "join"', function () {
+            var l1 = List.of([1, 2]);
+            var l2 = List.of([3, 4]);
+            var ll = List.of([l1, l2]);
+
+            var flatL = ll.join();
+
+            expect(List.is(flatL)).toBeTruthy();
+            expect(flatL.toJS()).toEqual([1, 2, 3, 4]);
+        });
+
+        it('fullfills the identity laws', function () {
+            // id === map(id)
+            expect(id(l).toJS()).toEqual(map(id)(l).toJS());
+
+            // compose(join, of) === compose(join, map(of)) === id
+            var m1 = compose(join, List.of)(l);
+            var m2 = compose(join, map(List.of))(l);
+
+            expect(m1.toJS()).toEqual(l.toJS());
+            expect(m2.toJS()).toEqual(l.toJS());
+        });
+
+        it('fullfills the composition law', function () {
+            // compose(map(f), map(g)) === map(compose(f,  g)
+            function f(x) { return 'f(' + x + ')'; }
+            function g(x) { return 'g(' + x + ')'; }
+            expect(compose(map(f), map(g))(l).toJS()).toEqual(map(compose(f, g))(l).toJS());
+        });
+
+        it('fullfills the associativity law', function () {
+            // compose(join, map(join)) === compose(join, join);
+            var l = List.of({ foo: 1 });
+            var mm = List.of(l);
+            var mmm = List.of(mm);
+
+            var r1 = compose(join, map(join))(mmm);
+            var r2 = compose(join, join)(mmm);
+
+            expect(r1.toJS()).toEqual(r2.toJS());
+        });
     });
 
-    it('allows to map list items without mutating the list', function () {
-        // prepare
-        var list = List.of([1, 2, 3]);
+    describe('debugging', function () {
+        it('has a custom string representation (for debugging)', function () {
+            expect('FOO=' + List.of([1, 3, 7])).toBe('FOO=List(1,3,7)');
+            expect(List.of([null, undefined, [1], {foo: 'bar'}, List.of(1)]).toString()).toBe('List(null,undefined,[1],{"foo":"bar"},List(1))');
+        });
 
-        // excute
-        var result = list.map(function (x) { return 2 * x; });
-
-        // verify
-        expect(list).not.toBe(result);
-        expect(list.join()).toEqual([1, 2, 3]);
-        expect(result.join()).toEqual([2, 4, 6]);
-    });
-
-    it('has a custom string representation (for debugging)', function () {
-        expect('FOO=' + List.of([1, 3, 7])).toBe('FOO=List([1,3,7])');
+        it('has a custom JSON representation', function () {
+            expect(JSON.stringify(List.of(List.of(List.of(1))))).toBe('"List(List(List(1)))"');
+        });
     });
 });
